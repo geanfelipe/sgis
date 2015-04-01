468a8d1f7e95875ed0e22b96483768fce00e72bf
Gean Felipe <geanfelipemoura@outlook.com>
2015-04-01 10:37:58 -0300

    commit tudo

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,84 @@ $(function(){
 		
 	});
 	$('.ui.dropdown').dropdown();
-}
+	$('.ui.form').form({
+    name: {
+      identifier  : 'name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your name'
+        }
+      ]
+    },
+    gender: {
+      identifier  : 'gender',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please select a gender'
+        }
+      ]
+    },
+    username: {
+      identifier : 'username',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a username'
+        }
+      ]
+    },
+    password: {
+      identifier : 'password',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    },
+    terms: {
+      identifier : 'terms',
+      rules: [
+        {
+          type   : 'checked',
+          prompt : 'You must agree to the terms and conditions'
+        }
+      ]
+    },
+    data: {
+      identifier : 'dataNascimentoPessoaFisica',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a date'
+        }
+      ]
+    },
 
+  });
 
-);
+jQuery.validator.addMethod("dataNascimentoPessoaFisica", function (value, element) {
+    //contando chars    
+    if (value.length != 10) return (this.optional(element) || false);
+    // verificando data
+    var data = value;
+    var dia = data.substr(0, 2);
+    var barra1 = data.substr(2, 1);
+    var mes = data.substr(3, 2);
+    var barra2 = data.substr(5, 1);
+    var ano = data.substr(6, 4);
+    if (data.length != 10 || barra1 != "/" || barra2 != "/" || isNaN(dia) || isNaN(mes) || isNaN(ano) || dia > 31 || mes > 12) return (this.optional(element) || false);
+    if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia == 31) return (this.optional(element) || false);
+    if (mes == 2 && (dia > 29 || (dia == 29 && ano % 4 != 0))) return (this.optional(element) || false);
+    if (ano < 1900) return (this.optional(element) || false);
+    return (this.optional(element) || true);
+}, "Informe uma data válida");  // Mensagem padrão 
+
+   $("#formPessoaFisica").validate();
+	});
